495c6e386acb82b4c4296b680fc42f69ceb428f1
Dylorix <66799677+Dylorix@users.noreply.github.com>
2022-12-15 18:02:45 +0200

    Add files via upload

---
+++ Бердников Лабораторная 6/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Sort (int[] array)
+        {
+            int i, j, min, temp;
+            for (i = 0; i < array.Length - 1; i++)
+            {
+                min = i; //Устанавливаем начальное значение минимального индекса
+
+                for (j = i + 1; j < array.Length; j++) //Находим индекс минимального элемента
+                {
+                    if (array[j] < array[min])
+                        min = j;
+                }
+
+                temp = array[i]; //Меняем значения старого мнимума и текущего местами
+                array[i] = array[min];
+                array[min] = temp;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+
+            int[] array = { 12, 11, -5, 0, 6, 34 }; //Сортируемая последовательность
+            Sort(array);
+            Console.WriteLine("Сортировка посредством выбора");
+
+            for (int i = 0; i < array.Length; i++) // Распечатываем отсортированный массив
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
