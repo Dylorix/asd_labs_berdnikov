c0c83517f20179b759a513fbb667270a1af02881
Dylorix <66799677+Dylorix@users.noreply.github.com>
2022-12-18 17:33:56 +0200

    Add files via upload

---
+++ Бердников Лабораторная 16/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bt = new BinaryTrees(8, null); //Добавляем значения в бинарное дерево
+            bt.Add(5);
+            bt.Add(3);
+            bt.Add(7);
+            bt.Add(13);
+            bt.Add(12);
+            bt.Add(17);
+            bt.Add(15);
+            bt.Add(16);
+            bt.Add(19);
+            string str = string.Empty;
+            bt.ByPassing(bt,ref str); //Нерекурсивный прямой обход
+            Console.WriteLine(str); //Его вывод
+
+            bt.print(); //Вывод линейно-скобочной записи
+        }
+    }
+}
